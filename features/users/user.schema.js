cc9cc824231147c9bb3a32a1430ce103085dec7d
Deependra Singh <deepshekhawat79999@gmail.com>
2024-04-05 13:43:07 +0530

    checking if node module can be excluded

---
+++ features/users/user.schema.js
@@ -0,0 +1,9 @@
+// Define the user and password variables
+const user = 'test_user';
+const password = 'test_password';
+
+// Export the userSchema object
+export const userSchema = {
+    user,
+    password,
+};
